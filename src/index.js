67b4cf3879c8bcafa3b6ce01c88cbdceaebd076b
Nekoguard <nekoguard@protonmail.com>
2021-03-12 19:35:44 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    let str = String(n);
+    let arr = str.split('').reverse(); // массив перевернут
+
+    for (let elem of arr) {
+        if (elem === '-') // если зашло отрицательное число
+        arr.pop(); // т.к. массив перевернут то '-' оказывается в конце и отрезается
+    }
+
+    let result = arr.join('');
+
+    return Number(result);
 }
